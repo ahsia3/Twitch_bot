eb25a8bd99ee4b3de4aa65fe2b22de533ce42aeb
ahsia3 <tonyhsia22@gmail.com>
2019-09-20 00:40:45 -0400

    Updates

---
+++ precache-manifest.6aa66bbb3e04ab256e5ed1b3b532dcb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee9cd28cb7ecfd0c1c39b4c4a7f8f2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef704a2c87a122213cf0",
+    "url": "/static/css/main.52af75fd.chunk.css"
+  },
+  {
+    "revision": "48c88652c7e4910bc6c0",
+    "url": "/static/js/2.809ceff5.chunk.js"
+  },
+  {
+    "revision": "ef704a2c87a122213cf0",
+    "url": "/static/js/main.31603fc5.chunk.js"
+  },
+  {
+    "revision": "b11d48a6d934fe40ee12",
+    "url": "/static/js/runtime-main.5f97ec04.js"
+  }
+]);
